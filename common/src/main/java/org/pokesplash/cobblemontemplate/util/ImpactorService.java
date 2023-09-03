737dc6a2cc939940cbc1d8acd0bb4a2051246bf1
bencrow11 <bencrow11@gmail.com>
2023-09-03 21:16:37 +0100

    Changed ImpactorService to be abstract with static methods.

--- common/src/main/java/org/pokesplash/cobblemontemplate/util/ImpactorService.java
+++ common/src/main/java/org/pokesplash/cobblemontemplate/util/ImpactorService.java
@@ -11,33 +11,23 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 /**
- * Class to interact with Impactor economy module.
+ * Class to interact with the Impactor API
  */
-public class ImpactorService {
+public abstract class ImpactorService {
 
-	// Economy service.
-	private EconomyService service;
-	// Currency to use.
-	private Currency currency;
+	// The impactor service
+	private static EconomyService service = EconomyService.instance();
 
-	/**
-	 * Constructor to create the Impactor service with the given currency.
-	 * @param currencyKey the key of the currency to use, pass null if you want the default.
-	 */
-	public ImpactorService(Key currencyKey) {
-		service = EconomyService.instance();
-
-		// If no currency is given, use the default from Impactor.
-		currency = currencyKey == null ? service.currencies().primary() :
-				service.currencies().currency(currencyKey).orElse(service.currencies().primary());
-	}
+	// The currency used for this mod.
+	private static Currency currency = service.currencies().primary();
 
 	/**
-	 * Method to get a users account for the currency stored.
-	 * @param uuid the uuid of the player
+	 * Method to get the account of a player.
+	 * @param uuid The UUID of the player.
 	 * @return The account of the player.
 	 */
-	public Account getAccount(UUID uuid) {
+
+	public static Account getAccount(UUID uuid) {
 		if (!service.hasAccount(uuid).join()) {
 			return null;
 		}
@@ -45,37 +35,37 @@ public class ImpactorService {
 	}
 
 	/**
-	 * Method to add a value to a users account.
-	 * @param account The account to add the value to
-	 * @param amount The amount to add to the account
-	 * @return true if successful.
+	 * Method to add to the balance of an account.
+	 * @param account The account to add the balance to.
+	 * @param amount The amount to add.
+	 * @return true if the transaction was successful.
 	 */
-	public boolean add(Account account, double amount) {
+	public static boolean add(Account account, double amount) {
 		EconomyTransaction transaction = account.depositAsync(new BigDecimal(amount)).join();
 
 		return transaction.successful();
 	}
 
 	/**
-	 * Method to remove value from an account
-	 * @param account The account to remove from
-	 * @param amount The amount to remove
-	 * @return true if the removal was successful.
+	 * Method to remove a balance from an account.
+	 * @param account The account to remove the balance from.
+	 * @param amount The amount to remove from the account.
+	 * @return true if the transaction was successful.
 	 */
-	public boolean remove(Account account, double amount) {
+	public static boolean remove(Account account, double amount) {
 		EconomyTransaction transaction = account.withdrawAsync(new BigDecimal(amount)).join();
 
 		return transaction.successful();
 	}
 
 	/**
-	 * Transfer money from one account to another
-	 * @param sender The player to send money from
-	 * @param receiver The player to receive the money
-	 * @param amount The amount to transfer
-	 * @return True if transfer was successful.
+	 * Method to transfer a balance from one account to another.
+	 * @param sender The account that is sending the balance.
+	 * @param receiver The account that is receiving the balance.
+	 * @param amount The amount to be transferred.
+	 * @return true if the transaction was successful.
 	 */
-	public boolean transfer(Account sender, Account receiver, double amount) {
+	public static boolean transfer(Account sender, Account receiver, double amount) {
 		EconomyTransferTransaction transaction = sender.transferAsync(receiver, new BigDecimal(amount)).join();
 
 		return transaction.successful();
